e9a301307e006186522d12f5adc227a3bd665c99
root <root@ip-172-31-24-149.ec2.internal>
2020-04-25 08:08:12 +0000

    First from the cloud

--- src/main/java/learning/blog/Bootstrap.java
+++ src/main/java/learning/blog/Bootstrap.java
@@ -1,63 +1,67 @@
-//package learning.blog;
-//
-//import learning.blog.models.Article;
-//import learning.blog.models.Comment;
-//import learning.blog.models.Role;
-//import learning.blog.models.User;
-//import learning.blog.repository.RoleRepository;
-//import learning.blog.service.ArticleService;
-//import learning.blog.service.UserService;
-//import learning.blog.service.impl.RoleService;
-//import org.hibernate.search.jpa.FullTextEntityManager;
-//import org.hibernate.search.jpa.Search;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.stereotype.Component;
-//
-//import javax.persistence.EntityManager;
-//import java.util.*;
-//
-//public class Bootstrap implements CommandLineRunner {
+// package learning.blog;
 
-//    private final ArticleService articleService;
-//    private final RoleService roleService;
-//    @Autowired
-//    UserService userService;
-//    @Autowired
-//    private RoleRepository roleRepository;
-//    public Bootstrap(ArticleService articleService, RoleService roleService) {
-//        this.articleService = articleService;
-//        this.roleService = roleService;
-//    }
-//    @Autowired
-//    EntityManager entityManager;
-//    @Override
-//    public void run(String... args) throws Exception {
-//        FullTextEntityManager fullTextEntityManager
-//                = Search.getFullTextEntityManager(entityManager);
-//        fullTextEntityManager.createIndexer().startAndWait();
-//        Article article = new Article();
-//        article.setAuthor("Prabhat");
-//        article.setCategory("dsa");
-//        article.setContent("sffds");
-//        article.setCreateDate(new Date());
-//        article.setTitle("sdgffg");
-//        Article savedArticle = articleService.save(article);
+// import learning.blog.models.Article;
+// import learning.blog.models.Comment;
+// import learning.blog.models.Role;
+// import learning.blog.models.User;
+// import learning.blog.repository.RoleRepository;
+// import learning.blog.service.ArticleService;
+// import learning.blog.service.UserService;
+// import learning.blog.service.impl.RoleService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.CommandLineRunner;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.stereotype.Component;
 
-//        User user = new User();
-//        user.setPassword(new BCryptPasswordEncoder().encode("ekdoteen"));
-//        user.setActive(true);
-//        user.setEmail("prabhat@gmail.com");
-//        Optional<Role> userRole = roleRepository.findById(1L);
-//        user.setRoles(new HashSet<Role>(Arrays.asList(userRole.get())));
-//        user.setName("Prabhat");
-//        user.setLastName("Sharma");
-//        user.setUsername("sahabji");
-//        List<Comment> holder = new ArrayList<>();
-//        user.setComments(holder);
-//        User savedUser = userService.saveAdmin(user);
-//       System.out.println("Saved Article  ");
-//        System.out.println("Saved user: " + savedUser.getUsername());
-//    }
-//}
+// import javax.persistence.EntityManager;
+// import java.util.*;
+
+// public class Bootstrap implements CommandLineRunner {
+
+//     private final ArticleService articleService;
+//     private final RoleService roleService;
+    
+//     // @Autowired
+//     // UserService userService;
+    
+    
+//     public Bootstrap(ArticleService articleService, RoleService roleService) {
+//         this.articleService = articleService;
+//         this.roleService = roleService;
+//     }
+//     @Autowired
+//     EntityManager entityManager;
+//     @Override
+//     public void run(String... args) throws Exception {
+        
+//         Article article = new Article();
+//         article.setAuthor("Prabhat");
+//         article.setCategory("dsa");
+//         article.setContent("This one is coming from the cloud");
+//         article.setCreateDate(new Date());
+//         article.setTitle("The first Article");
+//         Article savedArticle = articleService.save(article);
+
+//         Role role1 = new Role();
+//         //role1.setId(1l);
+//         role1.setRole("ADMIN");
+//         Role savedRole = roleService.save(role1);
+        
+//         System.out.println("Saved Role is : " + savedRole.getRole());
+
+//         // User user = new User();
+//         // user.setPassword(new BCryptPasswordEncoder().encode("ekdoteen"));
+//         // user.setActive(true);
+//         // user.setEmail("prabhat@gmail.com");
+//         // Optional<Role> userRole = roleRepository.findById(1L);
+//         // user.setRoles(new HashSet<Role>(Arrays.asList(userRole.get())));
+//         // user.setName("Prabhat");
+//         // user.setLastName("Sharma");
+//         // user.setUsername("sahabji");
+//         // List<Comment> holder = new ArrayList<>();
+//         // user.setComments(holder);
+//         // User savedUser = userService.saveAdmin(user);
+//         // System.out.println("Saved Article  ");
+//         //System.out.println("Saved user: " + savedUser.getUsername());
+//     }
+// }
